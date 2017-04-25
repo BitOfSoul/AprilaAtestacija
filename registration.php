d999cf30333a527657ceef5b9933d5b11a60c362
LanxyT1 <ruslan.rezanov2013@gmail.com>
2017-04-25 17:36:43 +0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -14,41 +14,52 @@
 				<aside class="col-sm-3"></aside>
 				<main class="col-sm-6">
 					<div class="form-horizontal" id="login">
-						<form action="event" method="post" id="eventForm">
 							 <fieldset>
 							 	<legend class="login_legend">Registration</legend>
 								<div class="row">
 								</div>
+								 
 								 <div class="form-group">
-				                	<label class="col-sm-3 control-label login_form" for="Username">Username:</label>
-				             		<div class="col-sm-9">
-				               			<input type="text" name="Username" class="form-control" id="Username" placeholder="Login"/>
-				              		</div>
-		            			</div>
+									<label class="col-sm-3 control-label login_form" for="password">Name:</label>
+									<div class="col-sm-9">
+									<input type="text" class="form-control" id="name" placeholder="name" name="name"/>
+								</div>
+								</div>
+								 <div class="form-group">
+									<label class="col-sm-3 control-label login_form" for="password">Surname:</label>
+									<div class="col-sm-9">
+									<input type="text" class="form-control" id="surname" placeholder="surname" name="surname"/>
+								</div>
+								</div>
+								 <div class="form-group">
+				                			<label class="col-sm-3 control-label login_form" for="Username">Username:</label>
+				             				<div class="col-sm-9">
+				               				<input type="text" name="Username" class="form-control" id="Username" placeholder="Login"/>
+				              			</div>
+		            					</div>
 								<div class="form-group">
 									<label class="col-sm-3 control-label login_form" for="password">Password:</label>
 									<div class="col-sm-9">
-										<input type="password" class="form-control" id="password" placeholder="Password" name="password"/>
+									<input type="password" class="form-control" id="password" placeholder="Password" name="password"/>
 									</div>
 								</div>
 								<div class="form-group">
-	                				<label class="col-sm-3 control-label login_form" for="passwordAgain">Rety your password:</label>
-	             					<div class="col-sm-9">
-	               						<input type="password" name="passwordAgain" class="form-control" id="passwordAgain">
-	              					</div>
-	            				</div>
+	                						<label class="col-sm-3 control-label login_form" for="passwordAgain">Rety your password:</label>
+	             							<div class="col-sm-9">
+	               							<input type="password" name="passwordAgain" class="form-control" id="passwordAgain">
+	              							</div>
+	            							</div>
 								<div class="form-group">
-								  <label class="col-sm-3 control-label login_form" for="email">Email:</label>
-								  <div class="col-sm-9">
-								    <input type="text" name="email" class="form-control" id="email" placeholder="E-mail">
-								  </div>
-								</div>
+								  	<label class="col-sm-3 control-label login_form" for="email">Email:</label>
+								  	<div class="col-sm-9">
+								    	<input type="text" name="email" class="form-control" id="email" placeholder="E-mail">
+								  	</div>
+									</div>
 
 							</fieldset>
 							<div class="button">
 								<button type="button" class="btn btn-success" id="send">Register</button>
 							</div>
-						</form>
 					</div>
 				</main>
 				<aside class="col-sm-3"></aside>
